2a407e865936835e688c16fa4c694df6de8c7282
Ary Kurnia <aryk922@gmail.com.com>
2023-08-13 23:16:47 +0700

    Penyelesaian

---
+++ pages/artikel.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../img/logo.png">
+        <link rel="stylesheet" href="../bootstrap-5.3.1/css/bootstrap.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Club Motor || Artikel</title>
+    </head>
+
+<body>
+    <div class="container-fluid d-flex">
+        <div class="row">
+            <div class="Collapsible col">
+                <div class="flex-shrink-0 p-3">
+                    <a href="../index.php"
+                        class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
+                        <img class="logo" src="../img/logo.png" alt="" srcset="">
+                        <span class="fs-5 fw-semibold">Rider Monkey</span>
+                    </a>
+                    <ul class="list-unstyled ps-0">
+                        <ul class=" nav  flex-column ">
+                            <li class="nav-item">
+                                <a href="../index.php" class="nav-link text-black">
+                                    Home
+                                </a>
+                            </li>
+                            <li class="border-top"></li>
+                            <li class="mb-1">
+                                <button
+                                    class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                    data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                                    Artikel
+                                </button>
+                                <div class="collapse" id="account-collapse">
+                                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                Artikel 1</a>
+                                        </li>
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                Artikel 2</a>
+                                        </li>
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                Artikel 3</a>
+                                        </li>
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                ...</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-10 ">
+            <div class="col-12 mt-5 m-5">
+                <header>
+                    <h1>Contoh Artikel Sederhana</h1>
+                    <p>Oleh: Nama Penulis</p>
+                </header>
+                <main>
+                    <article>
+                        <h2>Judul Artikel</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae tellus non turpis
+                            interdum cursus. Sed nec ante vel ex tristique lacinia.</p>
+                        <p>Phasellus non arcu ac libero fringilla eleifend. Fusce eget varius ligula, a tempor justo.
+                            Sed sit amet est non eros cursus vehicula.</p>
+                        <p>Quisque elementum ultrices urna. Nulla nec facilisis ligula. Sed fermentum hendrerit
+                            sagittis. Vivamus vel tincidunt mauris. Sed ullamcorper neque nec facilisis dapibus.</p>
+                    </article>
+                </main>
+                <footer>
+                    <p>Hak Cipta &copy; 2023 Nama Penulis</p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="../bootstrap-5.3.1/js/bootstrap.js"></script>
+
+</html>
---
+++ pages/galeri.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/logo.png">
+    <link rel="stylesheet" href="../bootstrap-5.3.1/css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Club Motor || Galeri</title>
+</head>
+
+<body>
+    <div class="container-fluid d-flex">
+        <div class="row">
+            <div class="Collapsible col">
+                <div class="flex-shrink-0 p-3">
+                    <a href="../index.php"
+                        class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
+                        <img class="logo" src="../img/logo.png" alt="" srcset="">
+                        <span class="fs-5 fw-semibold">Rider Monkey</span>
+                    </a>
+                    <ul class="list-unstyled ps-0">
+                        <ul class=" nav  flex-column ">
+                            <li class="nav-item">
+                                <a href="../index.php" class="nav-link text-black">
+                                    Home
+                                </a>
+                            </li>
+                            <li class="border-top"></li>
+                            <li class="mb-1">
+                                <button
+                                    class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                    data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                                    Galeri
+                                </button>
+                                <div class="collapse" id="account-collapse">
+                                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                Galeri 1</a>
+                                        </li>
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                Galeri 2</a>
+                                        </li>
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                Galeri 3</a>
+                                        </li>
+                                        <li><a href="#"
+                                                class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                                                ...</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-10">
+            <div class="col-12  m-5">
+                <h1 class="text-center">Galeri Foto</h1>
+                <div class="row">
+                    <div class="gal col-4">
+                        <img src="../img/compra-moto-usada.jpg" alt="" srcset="">
+                    </div>
+                    <div class="gal col-4">
+                        <img src="../img/contrato-moto.png" alt="" srcset="">
+                    </div>
+                    <div class="gal col-4">
+                        <img src="../img/footer_pilotaje.jpg" alt="" srcset="">
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="gal col-4">
+                        <img src="../img/portada-12.jpg" alt="" srcset="">
+                    </div>
+                    <div class="gal col-4">
+                        <img src="../img/portada-3.jpg" alt="" srcset="">
+                    </div>
+                    <div class="gal col-4">
+                        <img src="../img/portada-4.jpeg" alt="" srcset="">
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="gal col-4">
+                        <img src="../img/portada_enduro.jpg" alt="" srcset="">
+                    </div>
+                    <div class="gal col-4">
+                        <img src="../img/portada-3.jpg" alt="" srcset="">
+                    </div>
+                    <div class="gal col-4">
+                        <img src="../img/portada-4.jpeg" alt="" srcset="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="../bootstrap-5.3.1/js/bootstrap.js"></script>
+
+</html>
